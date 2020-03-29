7a24f92806e93d09bb86b5895a86ae8b8b8a083e
Aurelia Specker <aspecker@twitter.com>
2020-03-29 13:56:41 +0100

    Add API handler file and class for Premium Search API

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from authentication import Authentication
+from api_handler import Search_Counts
 
 def main():
     auth = Authentication()
     print(auth)
+
+    search_counts = Search_Counts(auth)
+    response = search_counts("from:AureliaSpecker")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
