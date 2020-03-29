5b65e216895e9d93ab382d458048577451d38969
Aurelia Specker <aspecker@twitter.com>
2020-03-29 14:42:16 +0100

    Add class for Labs Recent Search

--- authentication.py
+++ authentication.py
@@ -19,6 +19,11 @@ class Authentication:
         return OAuth1(self.CONSUMER_KEY, self.CONSUMER_SECRET,
                    self.ACCESS_TOKEN, self.TOKEN_SECRET,
                    signature_method="HMAC-SHA1")
+
+    def bearer_oauth(self, r):
+        r.headers['Authorization'] = f"Bearer {self.BEARER_TOKEN}"
+        r.headers['User-Agent'] = 'LabsResearchSearchQuickStartPython'
+        return r
     
     def __str__(self):
         return f"Consumer key: {self.CONSUMER_KEY} \nConsumer secret: {self.CONSUMER_SECRET} \nAccess token: {self.ACCESS_TOKEN} \nToken secret: {self.TOKEN_SECRET} \nBearer token: {self.BEARER_TOKEN}"
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from authentication import Authentication
-from api_handler import Search_Counts
+from api_handler import Search_Counts, Recent_Search_Data
 
 def main():
     auth = Authentication()
     print(auth)
 
     search_counts = Search_Counts(auth)
-    response = search_counts("from:AureliaSpecker")
-    print(response.text)
+    response_search_counts = search_counts("from:AureliaSpecker -is:retweet")
+    print(response_search_counts.text)
+
+    recent_search_data = Recent_Search_Data(auth)
+    response_recent_search_data = recent_search_data("from:AureliaSpecker -is:retweet")
+    print(response_recent_search_data.text)
 
 if __name__ == "__main__":
     main()
