e66cf7b4b45b4f514b549d3e2e0f826afba3f2df
Aurelia Specker <aspecker@twitter.com>
2020-04-20 08:05:11 +0100

    Set up flask for front end

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
+from app import app
 from twitter_guess_who import TwitterGuessWho
 
 def main():
-
-    tgw = TwitterGuessWho()
-    tgw()
-
-    # search_counts = Search_Counts(auth)
-    # response_search_counts = search_counts("from:AureliaSpecker -is:retweet")
-    # print(response_search_counts.text)
-    #
-    # recent_search_data = Recent_Search_Data(auth)
-    # response_recent_search_data = recent_search_data("from:AureliaSpecker -is:retweet")
-    # print(response_recent_search_data.text)
-
+    pass
+    # tgw = TwitterGuessWho()
+    # tgw()
+    
 if __name__ == "__main__":
     main()
